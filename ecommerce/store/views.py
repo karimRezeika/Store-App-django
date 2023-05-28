f52ee1016c1ce1d4a6d56b9ec9d7100a143834a1
karimRezeika <karimsaad948@gmail.com>
2023-05-29 01:48:32 +0300

    adding order summary to db

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -15,8 +15,16 @@ def cart(request):
         order = {'get_cart_total':0 , 'get_cart_items':0}
     context={'order':order , 'items':items}
     return render(request, 'store/cart.html',context)
+
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer , complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0 , 'get_cart_items':0}
+    context={'order':order , 'items':items}
     return render(request, 'store/checkout.html',context)
 
 
