c23f4986d93ffc375c4547bd7e8722ef9be2cded
karimRezeika <karimsaad948@gmail.com>
2023-05-29 01:32:20 +0300

    link cart with db

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -75,3 +75,5 @@ class ShippingAddress (models.Model):
     def __str__(self):
         return self.address
     
+
+
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -6,7 +6,14 @@ def store(request):
     context={'products':products}
     return render(request, 'store/store.html' , context)
 def cart(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer , complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0 , 'get_cart_items':0}
+    context={'order':order , 'items':items}
     return render(request, 'store/cart.html',context)
 def checkout(request):
     context={}
